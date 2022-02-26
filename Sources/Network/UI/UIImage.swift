0e4cc157c90a1d7f91819c10916e7dc1dcfa2bad
VeryLoveLoli <chuancan@icloud.com>
2022-02-26 11:45:41 +0800

    添加 UIImage 转 Image

---
+++ Sources/Network/UI/UIImage.swift
@@ -0,0 +1,20 @@
+//
+//  UIImage.swift
+//  
+//
+//  Created by 韦烽传 on 2022/2/26.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    /**
+     图片
+     */
+    public func image() -> Image {
+        
+        return Image(self)
+    }
+}
